1fdfe871f1a14e083108b621b926618e9f271ee1
William <william@WilliamdeMacBook-Pro.local>
2024-12-03 12:13:14 -0800

    Add the error handler middleware

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,12 +1,15 @@
 import express from 'express';
-import products from '../data/products.js';
+import Product from '../../database/models/productModel.js'
 import asyncHandler from '../middleware/asyncHandler.js';
+import { notFound, errorHandler } from '../middleware/errorHandler.js';
+
 const router = express.Router();
 
 // @desc Fetch all products
 // @route GET /api/products
 // @access Public
-router.get('/products', asyncHandler(async (req, res) => {
+router.get('/', asyncHandler(async (req, res) => {
+    const products = await Product.find({});
     res.json(products);
     })
 );
@@ -14,11 +17,20 @@ router.get('/products', asyncHandler(async (req, res) => {
 // @desc Fetch single product
 // @route GET /api/products/:id
 // @access Public
-router.get('/products/:id', asyncHandler(async (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
+router.get('/:id', asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+        res.json(product);
+    } else {
+        res.status(404);
+        throw new Error('Resource not found');
+    }
     })
 );
 
 
-export default productRoutes;
+
+router.use(notFound);
+router.use(errorHandler);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from '../database/config/db.js';
 import productRoutes from './routes/productRoutes.js';
-import products from './data/products.js';
+
 
 const port = process.env.PORT || 5001;
 
@@ -11,14 +11,15 @@ connectDB();
 
 const app = express();
 
+// basic route
 app.get('/', (req, res) => {
     res.send('App is running');
     }
 );
 
+// product routes
 app.use('/api/products', productRoutes);
 
-app.use()
 
 
 app.listen(port, () => {
