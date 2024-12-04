6090a717bfb71d93a36032132b6077a3784cf2a8
William <william@WilliamdeMacBook-Pro.local>
2024-12-03 19:27:32 -0800

    Implement the routes controller, seperate the routes and concrete operation

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,33 +1,12 @@
 import express from 'express';
-import Product from '../../database/models/productModel.js'
-import asyncHandler from '../middleware/asyncHandler.js';
+import { getProducts, getProductById } from '../controller/routesController.js';
 import { notFound, errorHandler } from '../middleware/errorHandler.js';
 
 const router = express.Router();
 
-// @desc Fetch all products
-// @route GET /api/products
-// @access Public
-router.get('/', asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-    })
-);
+router.get('/', getProducts);
 
-// @desc Fetch single product
-// @route GET /api/products/:id
-// @access Public
-router.get('/:id', asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-        res.json(product);
-    } else {
-        res.status(404);
-        throw new Error('Resource not found');
-    }
-    })
-);
-
+router.get('/:id', getProductById);
 
 
 router.use(notFound);
