430dba74070c737edd9f27fe37b17cf8a9d59391
William <william@WilliamdeMacBook-Pro.local>
2024-12-07 22:39:45 -0800

    Implement the JWT and HTTP only Cookie for user login route

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,3 +1,4 @@
+import jwt from 'jsonwebtoken';
 import asyncHandler from '../middleware/asyncHandler.js';
 import User from '../../database/models/userModel.js';
 
@@ -8,15 +9,26 @@ import User from '../../database/models/userModel.js';
 const authUser = asyncHandler(async (req, res) => {
     
     const { email, password } = req.body;
-    console.log('Request body:', req.body);
     const user = await User.findOne({ email });
 
     if (user && (await user.matchPassword(password))) {
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+            expiresIn: '30d',
+        });
+
+        res.cookie('jwt', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV !== 'development',
+            sameSite: 'strict',
+            maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+        });
+
         res.json({
             _id: user._id,
             name: user.name,
             email: user.email,
         });
+
     } else {
         res.status(401);
         throw new Error('Invalid email or password');
