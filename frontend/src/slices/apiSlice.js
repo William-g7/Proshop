f45503e094555f600e1606d01492e25efa6ffd4e
William <william@WilliamdeMacBook-Pro.local>
2024-12-19 14:55:23 -0800

    Fix a bug that cannot properly upload an image

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,17 +1,22 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { BASE_URL } from '../constants';
+import { logout } from './authSlice';
 
-const baseQuery = fetchBaseQuery({ 
+const baseQuery = fetchBaseQuery({
     baseUrl: BASE_URL,
-    credentials: 'include',
-    prepareHeaders: (headers) => {
-        headers.set('Content-Type', 'application/json');
-        return headers;
-    },
 });
 
+const baseQueryWithReauth = async (args, api, extra) => {
+    const result = await baseQuery(args, api, extra);
+    if (result.error && result.error.status === 401) {
+        api.dispatch(logout());
+    }
+    return result;
+};
+
+
 export const apiSlice = createApi({
-    baseQuery,
+    baseQuery: baseQueryWithReauth,
     tagTypes: ['Product', 'Order', 'User'],
     endpoints: (builder) => ({}),
 });
